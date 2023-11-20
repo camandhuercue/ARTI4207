b5a90e7358150ac3cc1c138e1a1258a96238455d
Camilo A. Huertas C <124012692+camandhuercue@users.noreply.github.com>
2023-11-19 19:41:40 -0500

    Update lambda_metadata.py

--- Paso 2/lambda_metadata.py
+++ Paso 2/lambda_metadata.py
@@ -17,20 +17,23 @@ def publish_topic(topic, message):
     client = boto3.client('sns')
     response = client.publish(TopicArn=topic, Message=message)
 
-
 def lambda_handler(event, context):
     if event:
         batch_item_failures = []
         sqs_batch_response = {}
-        print("Algo")
         for record in event["Records"]:
             try:
                 publish_topic(TOPIC, record['body'])
                 Object =  json.loads(record['body'])
                 Bin = get_object(BUCKET, Object['ID'] + "/" + Object['ID'] + ".csv")
                 HASH = hashlib.sha256(Bin).hexdigest()
-                print("Objeto: " + Object['ID'] + ", SHA256: " + HASH)
-                print(record['body'])
+                METADATA = {
+                    'HASH': HASH,
+                    'ID': ID,
+                    'OWNER': Object['OWNER'],
+                    'NAME': Object['NAME']
+                }
+                print(METADATA)
             except Exception as e:
                 print(str(e))
                 batch_item_failures.append({"itemIdentifier": record['messageId']})
