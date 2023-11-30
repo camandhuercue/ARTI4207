1c51f193e4084d159d5b2306f5b780d5636db9d4
Camilo A. Huertas C <124012692+camandhuercue@users.noreply.github.com>
2023-11-29 21:27:34 -0500

    Update main.py

--- Paso 1/main.py
+++ Paso 1/main.py
@@ -3,12 +3,16 @@ import csv
 from AWS_UTILS import aws_utils
 import os
 import json
+import time
 
-urllib.request.urlretrieve("https://raw.githubusercontent.com/camandhuercue/ARTI4207/main/DATASET.csv", "DATASET.csv")
+COUNT = 100
+#urllib.request.urlretrieve("https://raw.githubusercontent.com/camandhuercue/ARTI4207/main/DATASET.csv", "DATASET.csv")
+urllib.request.urlretrieve("https://www.datos.gov.co/resource/uzcf-b9dh.csv?audience=public&type=dataset", "DATASET.csv")
 BUCKET = ''
 SQS_URL = ''
 SODA = 'https://www.datos.gov.co/resource/{}.csv'
 
+start_time = time.time()
 with open('DATASET.csv') as csv_file:
 
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -21,14 +25,20 @@ with open('DATASET.csv') as csv_file:
             line_count += 1
         else:
             ID = row[0]
-            OWNER = row[1]
+            OWNER = row[3]
             NAME = row[2]
+            EMAIL = row[22]
+            CATEGORY = row[10]
             line_count += 1
             urllib.request.urlretrieve(SODA.format(ID), ID + '.csv')
             utils.s3_upload(ID + '.csv', BUCKET, ID + '/' + ID + '.csv')
             os.remove(ID + '.csv')
-            message = {'ID': ID, 'OWNER': OWNER, 'NAME': NAME}
+            message = {'ID': ID, 'OWNER': OWNER, 'NAME': NAME, 'EMAIL': EMAIL, 'CATEGORY': CATEGORY}
             MESSAGE = json.dumps(message)
+            #print(MESSAGE)
             utils.sqs_publish(SQS_URL, MESSAGE)
+        if line_count == (COUNT + 1):
+            break
 
-    print(f'Processed {line_count} lines.')
+    print(f'Processed {line_count - 1} lines.')
+    print("--- %s seconds ---" % (time.time() - start_time))
